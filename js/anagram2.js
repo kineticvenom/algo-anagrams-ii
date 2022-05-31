bbac24a9b82e38b690a3e3e209e319c4bdbbbd77
kineticvenom <kalebvarnes@yahoo.com>
2022-05-31 12:09:22 -1000

    Its Working

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,35 +1,23 @@
 // listOfWords = ["threads", "trashed", "hardest", "hatreds", "hounds"];
 
 exports.anagramsFor = function(word, listOfWords) {
-    
-    let tempList = listOfWords
-    
-    let wordSplit = word.replace(/[^\w]/g, "").toLowerCase().split("");
-    // console.logS(listOfWords,wordSplit)
-    for (w in listOfWords) {
-        console.log(w,listOfWords[w])
-      let wSplit = listOfWords[w].replace(/[^\w]/g, "").toLowerCase().split("");
-      console.log(wSplit)
-      let cnt = 0;
-    //   console.log(wordSplit.length, wSplit.length)
-      if (wordSplit.length, wSplit.length) {
-        for (i = 0; i < listOfWords.length; i++) {
-            // console.log(wSplit,i)
-          if (wSplit.includes(wordSplit[i])) {
-            cnt++;
-          }
+  let tempList = [];
+  let wordSplit = word.replace(/[^\w]/g, "").toLowerCase().split("");
+  for (w in listOfWords) {
+    let wSplit = listOfWords[w].replace(/[^\w]/g, "").toLowerCase().split("");
+    let cnt = 0;   
+    if (wordSplit.length === wSplit.length) {
+      for (i = 0; i < wSplit.length; i++) {
+        if (wSplit.includes(wordSplit[i])) {
+          cnt++;
         }
-      }//console.log(listOfWords)
-    //   console.log(cnt, wordSplit.length)
-      if (cnt !== wordSplit.length) {
-        tempList.splice(w, 1);
-        // console.log(tempList)
-        
       }
- 
-    } 
-    
-    listOfWords = tempList
-    return listOfWords
+      if (cnt === wordSplit.length) {
+        tempList.push(listOfWords[w])
+      }
+    }
+  }
+  listOfWords = tempList;
+  return listOfWords
   };
 
